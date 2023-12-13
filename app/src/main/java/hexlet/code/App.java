5c6153d8fbb3edfb699a6dbff235275b47905a75
smolyAS <smoljakov@mail.ru>
2023-12-13 21:17:27 +0300

    add --format

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,21 @@
 package hexlet.code;
+
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "gendiff", description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
+
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private String filePath1;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private String filePath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format")
+    private String format = "stylish";
 
     @Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
     private boolean helpRequested;
@@ -22,6 +33,8 @@ public class App implements Runnable {
             CommandLine.usage(this, System.out);
         } else if (versionRequested) {
             System.out.println("Version 1.0");
+        } else {
+            System.out.println("Comparing " + filePath1 + " and " + filePath2 + " using " + format + " format.");
         }
     }
 }
