760af4977fdbab10652643b693c2600cf936cb22
smolyAS <smoljakov@mail.ru>
2023-12-14 22:09:51 +0300

    step 5 v.1

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,36 +5,65 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-@Command(name = "gendiff", description = "Compares two configuration files and shows a difference.")
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+@Command(name = "app", mixinStandardHelpOptions = true, version = "1.0",
+        description = "Compares two configuration files and shows the difference.")
 public class App implements Runnable {
 
-    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    @Parameters(index = "0", description = "Path to the first file.")
     private String filePath1;
 
-    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    @Parameters(index = "1", description = "Path to the second file.")
     private String filePath2;
 
-    @Option(names = {"-f", "--format"}, description = "output format")
-    private String format = "stylish";
-
-    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
-    private boolean helpRequested;
-
-    @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
-    private boolean versionRequested;
-
     public static void main(String[] args) {
-        new CommandLine(new App()).execute(args);
+        CommandLine.run(new App(), args);
     }
 
     @Override
     public void run() {
-        if (helpRequested) {
-            CommandLine.usage(this, System.out);
-        } else if (versionRequested) {
-            System.out.println("Version 1.0");
-        } else {
-            System.out.println("Comparing " + filePath1 + " and " + filePath2 + " using " + format + " format.");
+        try {
+            String content1 = readFileContent(filePath1);
+            String content2 = readFileContent(filePath2);
+
+            // TODO: Perform comparison and generate output
+
+        } catch (IOException e) {
+            System.err.println("Error reading input files: " + e.getMessage());
         }
+    }
+
+    private String readFileContent(String filePath) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+        return new String(bytes);
+    }
+
+    public static String generate(Map<String, String> data1, Map<String, String> data2) throws Exception {
+        Set<String> allKeys = new TreeSet<>(data1.keySet());
+        allKeys.addAll(data2.keySet());
+
+        StringBuilder diffBuilder = new StringBuilder();
+
+        for (String key : allKeys) {
+            String value1 = data1.get(key);
+            String value2 = data2.get(key);
+
+            if (value1 == null) {
+                diffBuilder.append(key).append(" - ").append(value2).append(System.lineSeparator());
+            } else if (value2 == null) {
+                diffBuilder.append(key).append(" + ").append(value1).append(System.lineSeparator());
+            } else if (!value1.equals(value2)) {
+                diffBuilder.append(key).append(" - ").append(value2).append(System.lineSeparator());
+                diffBuilder.append(key).append(" + ").append(value1).append(System.lineSeparator());
+            }
+        }
+
+        return diffBuilder.toString();
     }
 }
