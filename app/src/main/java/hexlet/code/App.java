31a879ded934fc41859bdf4698cd8a60934b5cfe
smolyAS <smoljakov@mail.ru>
2024-02-03 22:47:02 +0300

    step 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,26 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "app", version = "1.0", description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    private boolean helpRequested;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    private boolean versionRequested;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CommandLine.run(new App(), args);
+    }
+
+    @Override
+    public void run() {
+        Differ differ = new Differ();
+        String result = differ.generate();
+        System.out.println(result);
     }
 }
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,8 @@
+package hexlet.code;
+
+public class Differ {
+    public static String generate() {
+        // логика метода generate
+        return null;
+    }
+}
