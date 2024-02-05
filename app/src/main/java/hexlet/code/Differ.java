b3b70639e6110044b1bf356680b10ca84a58889b
smolyAS <smoljakov@mail.ru>
2024-02-05 22:40:46 +0300

    step 5 v2

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -6,11 +6,47 @@ import com.google.gson.JsonParser;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.SortedMap;
-import java.util.TreeMap;
-import java.util.Map;
+import java.util.*;
 
 public class Differ {
+
+    public static String generateDiff(JsonObject json1, JsonObject json2) {
+        Map<String, String> resultMap = new TreeMap<>();
+
+        for (Map.Entry<String, JsonElement> entry : json1.entrySet()) {
+            String key = entry.getKey();
+            JsonElement value = entry.getValue();
+
+            if (json2.has(key)) {
+                if (!json2.get(key).equals(value)) {
+                    resultMap.put(key, "- " + key + ": " + value + System.lineSeparator() +
+                            "+ " + key + ": " + json2.get(key) + System.lineSeparator());
+                }
+                else {
+                    resultMap.put(key, "  " + key + ": " + value + System.lineSeparator());
+                }
+            } else {
+                resultMap.put(key, "- " + key + ": " + value + System.lineSeparator());
+            }
+        }
+
+        for (Map.Entry<String, JsonElement> entry : json2.entrySet()) {
+            String key = entry.getKey();
+            JsonElement value = entry.getValue();
+
+            if (!json1.has(key)) {
+                resultMap.put(key, "+ " + key + ": " + value + System.lineSeparator());
+            }
+        }
+
+        StringBuilder diffOutput = new StringBuilder();
+        for (String diffLine : resultMap.values()) {
+            diffOutput.append(diffLine);
+        }
+
+        return diffOutput.toString();
+    }
+
     public static String generate(String filePath1, String filePath2) {
         try {
             String content1 = new String(Files.readAllBytes(Paths.get(filePath1)));
@@ -19,54 +55,10 @@ public class Differ {
             JsonObject json1 = JsonParser.parseString(content1).getAsJsonObject();
             JsonObject json2 = JsonParser.parseString(content2).getAsJsonObject();
 
-            SortedMap<String, JsonElement> diff = generateDiff(json1, json2);
-
-            return formatDiff(diff);
+            return generateDiff(json1, json2);
         } catch (IOException e) {
             System.err.println("Error reading files: " + e.getMessage());
             return null;
         }
-    }
-
-    private static SortedMap<String, JsonElement> generateDiff(JsonObject json1, JsonObject json2) {
-        SortedMap<String, JsonElement> diff = new TreeMap<>();
-
-        for (Map.Entry<String, JsonElement> entry : json1.entrySet()) {
-            String key = entry.getKey();
-            JsonElement value1 = entry.getValue();
-            JsonElement value2 = json2.get(key);
-
-            if (value2 == null) {
-                diff.put("- " + key, value1);
-            } else if (value2.equals(value1)) {
-                diff.put("  " + key, value1);
-            } else {
-                diff.put("- " + key, value1);
-                diff.put("+ " + key, value2);
-            }
-        }
-
-        for (Map.Entry<String, JsonElement> entry : json2.entrySet()) {
-            String key = entry.getKey();
-            JsonElement value2 = entry.getValue();
-            JsonElement value1 = json1.get(key);
-
-            if (value1 == null) {
-                diff.put("+ " + key, value2);
-            }
-        }
-
-        return diff;
-    }
-
-    private static String formatDiff(SortedMap<String, JsonElement> diff) {
-        StringBuilder formattedOutput = new StringBuilder();
-
-        for (Map.Entry<String, JsonElement> entry : diff.entrySet()) {
-            String prefix = entry.getKey().startsWith(" ") ? "" : " ";
-            formattedOutput.append(prefix).append(entry.getKey()).append(": ").append(entry.getValue()).append(System.lineSeparator());
-        }
-
-        return formattedOutput.toString().trim();
     }
 }
